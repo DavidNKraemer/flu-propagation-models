3984f0fee260759a2320bdf6514b0e4cb59ba5df
kraemerd17 <kraemerd17@grinnell.edu>
2016-04-11 01:57:53 -0500

    Some more documentation, and a new gif.

--- animation.py
+++ animation.py
@@ -13,7 +13,7 @@ percent_dead = 0.0
 people = []
 
 # simulation parameters
-iterations = 600
+iterations = 1000
 movement_speed = 1.00e-2
 radius = 0.35
 
@@ -34,25 +34,33 @@ remove_coords = ax.plot([], [], label='Remove')[0]
 dead_coords = ax.plot([], [], label='Dead')[0]
 
 def init():
+    # initialize the populations
     susceptibles = [Person(SUSCEPTIBLE) for _ in range(num_susceptibles)]
-    infectives = [Person(INFECTIVE) for _ in range(num_infectives)]
-    removes = [Person(REMOVE) for _ in range(num_removes)]
-    dead = [Person(DEAD) for _ in range(num_dead)]
+    infectives   = [Person(INFECTIVE)   for _ in range(num_infectives)]
+    removes      = [Person(REMOVE)      for _ in range(num_removes)]
+    dead         = [Person(DEAD)        for _ in range(num_dead)]
 
+    # track the total population in one list (pointers to Person objects)
     global people 
     people = susceptibles + infectives + removes + dead
 
+    # start with blank data
     susceptible_coords.set_data([], [])
     infective_coords.set_data([], [])
     remove_coords.set_data([], [])
     dead_coords.set_data([], [])
+
     return susceptible_coords, infective_coords, remove_coords, dead_coords
 
 def update_coordinates(coordinate_obj, data, color, legend):
+
+    # check to make sure that the data is not empty
     if len(np.shape(data)) == 2:
         coordinate_obj.set_data(data[:,0], data[:,1])
     else:
         coordinate_obj.set_data([], [])
+
+    # data styling
     coordinate_obj.set_label(legend)
     coordinate_obj.set_alpha(0.5)
     coordinate_obj.set_marker('o')
@@ -62,6 +70,8 @@ def update_coordinates(coordinate_obj, data, color, legend):
 
 def animate(iteration):
     global people, box
+
+    # identify coordinates for each group of Person objects
     susceptibles = np.array([person.loc for person in people \
             if person.status is SUSCEPTIBLE])
     infectives = np.array([person.loc for person in people \
@@ -71,21 +81,26 @@ def animate(iteration):
     dead = np.array([person.loc for person in people \
             if person.status is DEAD])
 
+    # update the figure with new coordinates and statuses
     update_coordinates(susceptible_coords, susceptibles, 'green', 'Susceptible')
     update_coordinates(infective_coords, infectives, 'red', 'Infective')
     update_coordinates(remove_coords, removes, 'blue', 'Remove')
     update_coordinates(dead_coords, dead, 'black', 'Dead')
+
+    # adjust the plot so that the legend is off to the bottom
     ax.set_position([box.x0, box.y0 + box.height * 0.1,
                          box.width, box.height * 0.9])
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
                   fancybox=True, shadow=True, ncol=5)
 
+    # actually update the simulation
     for person in people:
         person.update_status(people, radius)
         person.update_location(movement_speed)
 
     return susceptible_coords, infective_coords, remove_coords, dead_coords
 
+# make an animation!
 ani = animation.FuncAnimation(fig, animate, frames=iterations, interval=0, 
         blit=True, init_func=init)
 
