d5c738a2d32b733b709821ccd087680d193cfa8d
Caleb Leedy <leedycal16@math.grinnell.edu>
2016-04-12 15:46:21 -0500

    Add Fixed-step solvers

--- Baseline_Model.m
+++ Baseline_Model.m
@@ -1,16 +1,13 @@
 % This is the Disease model for Project 3 in MAT 306 Mathematical Modeling
 % Evan Christianson, David Kraemer, Caleb Leedy, and Will Royle
 
-% TODO: change our differential equation solver to a fixed step method
-
-
 syms infectives susceptible removes dead
 
 % **********************
 % * Initial Conditions *
 % **********************
 
-total_population = 10;
+total_population = 100;
 
 % Initial Populations for each group
 % These are given as proportions of the total population
@@ -28,8 +25,10 @@ contagion_rate = 0.02;
 recovery_rate = 0.1;
 resistance_rate = 0.5;
 lost_immunity_rate = 0.1;
-fatalality_rate = 0.03;
+fatalality_rate = 0.0003;
 
+% Time Values for a Fixed Time Solver
+tvalues = 0:1:100;
 
 % ***************
 % * Basic Model *
@@ -39,7 +38,7 @@ df = contagion_rate*infectives*(1-infectives-removes)*total_population - recover
 dr = 0;
 ds = -df;
 
-[T, Y] = ode45(@(t,y) double(subs([df; dr; ds],[infectives removes susceptible],[y(1) y(2) y(3)])),[0 100], [initial_infectives initial_removes initial_susceptible])
+[T, Y] = ode45(@(t,y) double(subs([df; dr; ds],[infectives removes susceptible],[y(1) y(2) y(3)])),tvalues, [initial_infectives initial_removes initial_susceptible])
 
 figure
 hold off
@@ -63,7 +62,7 @@ ds = -df - dr;
 
 FixPtsIM = vpasolve([df == 0; dr == 0; susceptible+removes+infectives == 1], [infectives, removes, susceptible])
 
-[T2, Y2] = ode45(@(t,y) double(subs([df; dr; ds],[infectives removes susceptible],[y(1) y(2) y(3)])),[0 100], [initial_infectives initial_removes initial_susceptible])
+[T2, Y2] = ode45(@(t,y) double(subs([df; dr; ds],[infectives removes susceptible],[y(1) y(2) y(3)])),tvalues, [initial_infectives initial_removes initial_susceptible])
 
 figure
 hold off
@@ -82,7 +81,7 @@ ds = -df - dr;
 FixPtsIMLI = vpasolve([df == 0; dr == 0; susceptible+removes+infectives == 1], [infectives, removes, susceptible])
 
 
-[T3, Y3] = ode45(@(t,y) double(subs([df; dr; ds],[infectives removes susceptible],[y(1) y(2) y(3)])),[0 100], [initial_infectives initial_removes initial_susceptible])
+[T3, Y3] = ode45(@(t,y) double(subs([df; dr; ds],[infectives removes susceptible],[y(1) y(2) y(3)])),tvalues, [initial_infectives initial_removes initial_susceptible])
 
 figure
 hold off
@@ -94,22 +93,17 @@ legend('infectives', 'removes', 'susceptibles')
 % * Printing the differences between Model 2 and Model 3 *
 % ********************************************************
 
-test1 = zeros(length(Y3));
-test2 = zeros(length(Y3));
+testplot = zeros(length(Y3),3);
 
-%TODO: Change this
-
-for i = 1:length(Y2)
-    test1(i) = Y3(i,1) - Y2(i,1);
-    test2(i) = Y3(i,2) - Y2(i,2);
-    test3(i) = Y3(i,3) - Y2(i,3);
+for i = 1:3
+    testplot(:,i) = Y3(:,i) - Y2(:,i);
 end
 
-figure
-hold on
-plot(test1, 'b')
-plot(test2, 'g')
-plot(test3, 'r')
+figure 
+hold off
+plot(T3,testplot)
+axis([0 100 0 1])
+
 
 % **************************
 % * Model with Dead People *
@@ -123,7 +117,7 @@ ds = -df - dr - dd;
 FixPtsD = vpasolve([df == 0; dr == 0; dd == 0; susceptible+removes+infectives == 1], [infectives, removes, dead, susceptible])
 
 
-[T4, Y4] = ode45(@(t,y) double(subs([df; dr; dd; ds],[infectives removes dead susceptible],[y(1) y(2) y(3) y(4)])),[0 100], [initial_infectives initial_removes initial_dead initial_susceptible]);
+[T4, Y4] = ode45(@(t,y) double(subs([df; dr; dd; ds],[infectives removes dead susceptible],[y(1) y(2) y(3) y(4)])),tvalues, [initial_infectives initial_removes initial_dead initial_susceptible]);
 
 figure
 hold off
