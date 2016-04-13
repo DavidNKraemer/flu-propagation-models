fec3adf1f492dd95777996a6045bf3b25bbb3988
Caleb Leedy <leedycal16@math.grinnell.edu>
2016-04-12 23:50:45 -0500

    Added radius to models

--- runge_kutta4.m
+++ runge_kutta4.m
@@ -1,8 +1,11 @@
 function [t, y] = runge_kutta4(f, t0, y0, tn, h)
+disp(size(y0)); % 3x1
+disp(size(f(t0,y0))); % 3x1
 n = int16((tn - t0) / h);
-eqns = size(y0);
+eqns = size(y0); % [3 1]
+disp(eqns(1));
 t = [t0 zeros(1, n)];
-y = [y0.' zeros(eqns(2), n)];
+y = [y0 zeros(eqns(1), n)];
 for i = 2:n+1
     k1 = f(t(i-1),y(:, i-1)).';
     k2 = f(t(i-1) + h / 2, y(:, i-1) + h * k1 / 2).';
